1e9b5dde3c261c4ed7749dc9696077043d9727c0
idbic <dbicga@gmail.com>
2023-02-03 12:55:19 -0700

    added all items with icons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import styled from 'styled-components'
 import thelogo from '../img/logo.png'
-
+import HomeIcon from '@mui/icons-material/Home'
+import ExploreOutlinedIcon from '@mui/icons-material/Explore'
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined'
+import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined'
+import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined'
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+import {Link} from 'react-router-dom'
 const Container = styled.div`
     flex:1;
     background-color: #202020;
     height: 100vh;/
-    color: white
+    color: white;
+    font-size: 10px;
 
 
 `
@@ -19,6 +36,8 @@ const Logo = styled.div`
     display: flex;
     align-items: center;
     gap: 5px;
+    font-weight: bold;
+    margin-bottom: 25px;
 
 
 `
@@ -26,6 +45,40 @@ const Logo = styled.div`
 const Img = styled.img`
     height: 25px;
 `
+const Item = styled.div`
+display: flex;
+align-items: center;
+gap: 20px;
+cursor: pointer;
+`
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Login = styled.div``;
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 20px;
+`;
+
 
 export default function Menu (){
     return(
@@ -35,6 +88,74 @@ export default function Menu (){
                     <Img src={thelogo}/>
                     YerTURB.
                 </Logo>
+                <Item>
+                    <HomeIcon/> 
+                    Home
+                </Item>
+                <Item>
+                    <ExploreOutlinedIcon /> 
+                    Explore
+                </Item>
+                <Item>
+                    <SubscriptionsOutlinedIcon/> 
+                    Subscriptions.
+                </Item>
+                <Item>
+                    <HistoryOutlinedIcon /> 
+                    History
+                </Item>
+                <Login>
+          Sign in to like videos, comment, and subscribe.
+         <Item>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+            </Item>
+        </Login>
+        <Hr />
+        <Title>BEST OF LAMATUBE</Title>
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Gaming
+        </Item>
+        <Item>
+          <MovieOutlinedIcon />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlinedIcon />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
+        <Item >
+          <SettingsBrightnessOutlinedIcon />
+          Mode
+        </Item>
             </Wrapper>
         </Container>
     )
