943f5df724d47bfde8ec9d65980246fade9f919d
idbic <dbicga@gmail.com>
2023-02-06 00:20:28 -0700

    added a few updates to the video player page

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,3 +1,4 @@
+// import { Title } from '@mui/icons-material';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -10,7 +11,19 @@ flex: 5;`;
 const VideoWrapper = styled.div`
 `;
 
-const Details = styled.div``;
+const Title = styled.h1`
+font-size: 18px;
+font-weight: 400;
+margin-top: 20px;
+margin-bottom: 10px;
+color: ${({theme})=>theme.text};
+
+`
+const Details = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+`;
 const Info = styled.span``;
 const Buttons = styled.div``;
 const Button = styled.div``;
@@ -26,8 +39,21 @@ export default function Video(){
         <Container>
             <Content>
             <VideoWrapper>
-              Content  
+            <iframe width="560" height="315" 
+            src="https://www.youtube.com/embed/GknZcxCyNMM" 
+            title="YouTube video player" 
+            frameborder="0" allow="accelerometer; 
+            autoplay; clipboard-write; encrypted-media; 
+            gyroscope; picture-in-picture; web-share" 
+            allowfullscreen></iframe> 
             </VideoWrapper>
+            <Title>Test Video </Title>
+            <Details>
+                <Info>400 views - Nov 21 2033</Info>
+                <Buttons>
+                    <Button>Button</Button>
+                </Buttons>
+            </Details>
             </Content>
             <Recommended>
                 Recommended
